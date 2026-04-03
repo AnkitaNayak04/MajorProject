0da1fe643c9fb094bdddde28d2611edace1f1292
AnkitaNayak04 <ankitaanayak0604@gmail.com>
2026-04-03 17:09:03 +0530

    initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/giet_deptconnect");
-    console.log("MongoDB connected to giet_deptconnect");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected to Atlas");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
     process.exit(1);
